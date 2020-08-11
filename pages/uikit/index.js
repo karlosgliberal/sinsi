01ab210ce563e15088cd2cabfc86c7378fb9f5ee
Aitor Resano <aitor.rl@gmail.com>
2020-08-11 14:35:13 +0200

    Tengo una escala de espaciados, color de fondo y empiezo a ocupar toda tu pantalla

--- pages/uikit/index.js
+++ pages/uikit/index.js
@@ -1,5 +1,5 @@
 export default () => (
-  <div className="p-4 bg-white rounded shadow">
+  <div className="p-4 bg-sinsiblue w-screen h-screen">
     <h1 className="text-2xl font-bold">Soy el uikit de Aitor</h1>
     <p className="text-gray-500">Y aqui estamos en un parrafo</p>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,28 @@
 module.exports = {
   purge: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
+
+    spacing: {
+      px: '1px',
+      '0': '0',
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '3': '0.75rem',
+      '4': '1rem',
+      '5': '1.25rem',
+      '6': '1.5rem',
+      '8': '2rem',
+      '10': '2.5rem',
+      '12': '3rem',
+      '16': '4rem',
+      '20': '5rem',
+      '24': '6rem',
+      '32': '8rem',
+      '40': '10rem',
+      '48': '12rem',
+      '56': '14rem',
+      '64': '16rem',
+    },
     screens: {
       sm: '640px',
       md: '768px',
@@ -10,6 +32,7 @@ module.exports = {
     extend: {
       colors: {
         purple: '#6C03AB',
+        sinsiblue: '#16242D',
       },
     },
   },

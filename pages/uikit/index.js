b18e6857e0c39adc7ce82f9edf87ac06be2d2fae
Aitor Resano <aitor.rl@gmail.com>
2020-08-11 14:46:44 +0200

    Quiero mis moléculas

--- pages/uikit/index.js
+++ pages/uikit/index.js
@@ -1,6 +1,23 @@
 export default () => (
   <div className="p-4 bg-sinsiblue w-screen h-screen">
-    <h1 className="text-2xl font-bold">Soy el uikit de Aitor</h1>
-    <p className="text-gray-500">Y aqui estamos en un parrafo</p>
+
+    <h1 className="text-3xl font-bold text-white">UIKIT</h1>
+    <div>
+      <ul>
+        <li class="py-4">
+          <a class="text-white" href="">
+            Textos
+        </a>
+
+        </li>
+        <li class="py-4">
+          <a class="text-white" href="">
+            Inputs
+        </a>
+        </li>
+
+      </ul>
+    </div>
+
   </div>
 );

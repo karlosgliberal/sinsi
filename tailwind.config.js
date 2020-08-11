a53b7b7b7570cfc8e38a103a2814ee59a9378a56
Aitor Resano <aitor.rl@gmail.com>
2020-08-11 13:52:55 +0200

    Primer commit este será mi esqueleto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   purge: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {},
   },
   variants: {},

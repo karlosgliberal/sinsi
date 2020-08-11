1a2a8474c14e6dce9be33df8b5c542bf59f73d3e
karlosgliberal <karlosgliberal@gmail.com>
2020-08-11 12:48:51 +0200

    La personalidad tiene visiones multiples, pero se tiene que hacer por partes

---
+++ pages/uikit/index.js
@@ -0,0 +1,6 @@
+export default () => (
+  <div className="p-4 bg-white rounded shadow">
+    <h1 className="text-2xl font-bold">Soy el uikit de Aitor</h1>
+    <p className="text-gray-500">Y aqui estamos en un parrafo</p>
+  </div>
+);

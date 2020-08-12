73c98435304e985b1e48a6dcc9666976d0fe280a
Aitor Resano <aitor.rl@gmail.com>
2020-08-12 10:35:11 +0200

    Tengo otro componente mas y ademas soy fluido

--- pages/uikit/index.js
+++ pages/uikit/index.js
@@ -1,31 +1,40 @@
 export default () => (
-  <div className="p-4 bg-sinsiblue w-screen h-screen">
+  <div className="p-4 bg-sinsiblue w-screen h-screen flex justify-center">
 
-    <h1 className="text-3xl font-bold text-white">UIKIT</h1>
-    <div>
-      <ul>
-        <li class="py-4">
-          <a class="text-white" href="">
-            Textos
-        </a>
+    <div class="w-1/2 border border-gray-700">
 
-        </li>
-        <li class="py-4">
-          <a class="text-white" href="">
-            Inputs
-        </a>
-        </li>
 
-      </ul>
+      <div class="p-4">
+        <h1 className="text-3xl font-bold text-white">UIKIT</h1>
+      </div>
+
+      <div class="p-4">
+        <h2 class="text-2xl text-white">TEXTOS</h2>
+
+      </div>
+      <div>
+        <p class="font-sinsimono p-6 text-white">
+          Vivamus blandit arcu eget tempor ullamcorper. Integer porta nibh at condimentum tempus. Mauris interdum neque vitae est ultricies pretium. Proin in lectus vitae tellus auctor fermentum aliquam vel velit. Aliquam risus augue, finibus at volutpat malesuada, vestibulum eu mauris. Mauris efficitur elit a tortor sollicitudin tristique. Suspendisse fringilla congue est id suscipit.
+      </p>
+        <p class="text-white p-6">
+          Vivamus blandit arcu eget tempor ullamcorper. Integer porta nibh at condimentum tempus. Mauris interdum neque vitae est ultricies pretium. Proin in lectus vitae tellus auctor fermentum aliquam vel velit. Aliquam risus augue, finibus at volutpat malesuada, vestibulum eu mauris. Mauris efficitur elit a tortor sollicitudin tristique. Suspendisse fringilla congue est id suscipit.
+      </p>
+      </div>
+      <div class="border-t-2 border-b-2 border-gray-800 p-3 m-4 grid grid-cols-2">
+        <div class="">
+          <input type="text" placeholder="escribe tu texto"></input>
+        </div>
+        <div class="border-r-2 border-gray-800 ">
+          .
+        </div>
+
+      </div>
+
+
+
     </div>
-    <div>
-      <p class="font-sinsimono p-6 text-white">
-        Vivamus blandit arcu eget tempor ullamcorper. Integer porta nibh at condimentum tempus. Mauris interdum neque vitae est ultricies pretium. Proin in lectus vitae tellus auctor fermentum aliquam vel velit. Aliquam risus augue, finibus at volutpat malesuada, vestibulum eu mauris. Mauris efficitur elit a tortor sollicitudin tristique. Suspendisse fringilla congue est id suscipit.
-  </p>
-      <p class="text-white p-6">
-        Vivamus blandit arcu eget tempor ullamcorper. Integer porta nibh at condimentum tempus. Mauris interdum neque vitae est ultricies pretium. Proin in lectus vitae tellus auctor fermentum aliquam vel velit. Aliquam risus augue, finibus at volutpat malesuada, vestibulum eu mauris. Mauris efficitur elit a tortor sollicitudin tristique. Suspendisse fringilla congue est id suscipit.
-  </p>
-    </div>
+
+
   </div>
 
 );

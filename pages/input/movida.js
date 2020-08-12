2a20976c675d2cf5aadea051ecdc79c7f2cdcaae
karlosgliberal <karlosgliberal@gmail.com>
2020-08-12 18:15:45 +0200

    Se compian unos a otros para hacer lo mismo

--- pages/input/movida.js
+++ pages/input/movida.js
@@ -18,17 +18,43 @@ export default class NameForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="p-4 bg-sinsiblue w-screen h-screen flex justify-center">
+        <div className="w-1/2 border border-gray-700 flex flex-col justify-end bg-dots">
+          <div className="p-4">
+            <h1 className="text-3xl font-bold text-white">UIKIT</h1>
+          </div>
+
+          <div className="p-4">
+            <h2 className="text-2xl text-white">TEXTOS</h2>
+          </div>
+          <p className="text-white p-6">
+            VivamDus blandit arcu eget tempor ullamcorper. Integer
+          </p>
+          <div className="text-decoration">
+            <p className="font-sinsimono p-6 text-white">
+              Blandit arcu eget tempor ullamcorper. Integer porta nibh at
+              condimentum tempus. Mauris interdum neque vitae est ultricies
+              pretium. Proin in lectus vitae tellus auctor fermentum aliquam vel
+              velit. Aliquam risus augue, finibus at volutpat malesuada,
+              vestibulum eu mauris.
+            </p>
+          </div>
+
+          <p className="text-right font-sinsimono p-6 text-sinsipurple">
+            Morado
+          </p>
+          <div className="border-t-2 border-b-2 border-gray-800 p-1 mb-3 mx-4 grid grid-cols-submit">
+            <div className="">
+              <input
+                type="text"
+                className="border-0 bg-transparent w-full focus:border-transparent py-3 px-2 rounded-none"
+                placeholder="Escribe tu texto"
+              ></input>
+            </div>
+            <div className="border-l-2 border-gray-800 ">{/* Submit */}</div>
+          </div>
+        </div>
+      </div>
     );
   }
 }

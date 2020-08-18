293134f4555f4a87a0b1a710d9916220f3cb05c5
Aitor Resano <aitor.rl@gmail.com>
2020-08-18 16:51:40 +0200

    Dots dots dots

--- pages/uikit/bg.js
+++ pages/uikit/bg.js
@@ -9,13 +9,13 @@ export default () => (
                     <h1 className="text-5xl font-bold text-white">FONDOS</h1>
                 </div>
 
-                <div class="h-40 w-full bg-type-1 my-4">
+                <div class="h-40 w-full bg-type-1 m-4">
 
                 </div>
-                <div class="h-40 w-full bg-type-2 my-4">
+                <div class="h-40 w-full bg-type-2 m-4">
 
                 </div>
-                <div class="h-40 w-full bg-type-3 my-4">
+                <div class="h-40 w-full bg-type-3 m-4">
 
                 </div>
 

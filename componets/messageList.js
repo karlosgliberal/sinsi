66588551f709d9573a4cf6da935533b437e03da2
karlosgliberal <karlosgliberal@gmail.com>
2020-08-23 12:59:41 +0200

    Jugando con el chat

--- componets/messageList.js
+++ componets/messageList.js
@@ -1,14 +1,17 @@
+import { loadGetInitialProps } from 'next/dist/next-server/lib/utils';
+
 export default class MessageList extends React.Component {
   render() {
     return (
       <div className="message-list">
         {this.props.messages.map((message, index) => {
+          console.log(message);
           return (
             <p
               key={Math.random() * (12 - 2) + 2}
               className="font-sinsimono p-6 text-white"
             >
-              {message}
+              {message.txt}
             </p>
           );
         })}

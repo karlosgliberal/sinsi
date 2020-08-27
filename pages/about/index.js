e38f3fac36b5b32f2e5453e78ea0931348d837f2
Aitor Resano <aitor.rl@gmail.com>
2020-08-27 12:28:49 +0200

    Me veo muy bien en todas las pantallas y ademas tengo un About, Sinsi siempre ha querido tener un about...

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,27 @@
 export default () => (
-  <div className="p-4 bg-white rounded shadow">
-    <h1 className="text-2xl font-bold">About</h1>
-    <p className="text-gray-500">BOBO</p>
+  <div className="bg-sinsiblue w-screen h-screen flex justify-center">
+    <div className="w-screen lg:w-1/2 border border-gray-700 flex flex-col justify-center bg-dots">
+      <div className="p-4">
+        <a className="text-white hover:text-sinsipurple transition duration-700" href="/">&#8592;</a>
+      </div>
+      <div className="h-auto overflow-scroll flex flex-col justify-end">
+        <div className="flex flex-row justify-center px-4 my-10 ">
+          <img src="/images/logos.svg"></img>
+        </div>
+        <div class="p-6">
+          <p className="font-sinsimono text-white">
+            Sinsi es nuesta app de Diseño de futuros y fue ideada por los magistrales.... Integer porta nibh at
+            condimentum tempus. Mauris interdum neque vitae est ultricies pretium.
+            Proin in lectus vitae.
+            mauris.
+        </p>
+          <p className="font-sinsimono text-white">
+            us augue, finibus at volutpat malesuada, vestibulum eu
+            mauris.
+        </p>
+        </div>
+      </div>
+
+    </div>
   </div>
 );

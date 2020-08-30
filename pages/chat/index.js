a49763eedfd34e0616cf8bc808c724ef3dc9438d
karlosgliberal <karlosgliberal@gmail.com>
2020-08-30 22:29:35 +0200

    Ahora ya no soy de ningúna clase, ni de nigún tipo y tengo efectos, y afectos

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,68 +1,64 @@
+import { useState, useEffect } from 'react';
 import MessageList from '../../componets/MessageList';
 import MessageForm from '../../componets/MessageForm';
 import ButtonList from '../../componets/ButtonList';
 import { getIntentionFromDialogflow } from '../../services/dialogflowResponse';
 import { sinsiText } from '../../core/sinsiText';
 
-export default class Chat extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sinsiText,
-      boton: true,
-      messages: [{ author: 'Them', body: 'Hey there!' }],
-    };
-  }
+export default function Chat(props) {
+  const [menssagesLista, setMenssageList] = useState([]);
 
-  getIntention = async intention => {
+  const getIntention = async intention => {
     const res = await getIntentionFromDialogflow(intention);
-    this.setState({
-      messages: [
-        ...this.state.messages,
-        { me: true, author: 'You', body: res.data.fulfillmentText },
-      ],
-    });
-  };
-  handleButtonClick = event => {
-    this.setState({
-      messages: [
-        ...this.state.messages,
-        { me: true, author: 'You', body: event },
-      ],
-    });
+    setMenssageList(menssagesLista => [
+      ...menssagesLista,
+      {
+        author: 'You',
+        body: res.data.fulfillmentText,
+      },
+    ]);
   };
 
-  componentDidMount() {
-    this.getIntention('dame la entrada');
-  }
-
-  handleNewMessage = text => {
-    this.setState({
-      messages: [
-        ...this.state.messages,
-        { me: true, author: 'Me', body: text },
-      ],
-    });
-    this.getIntention(text);
+  const handleButtonClick = event => {
+    setMenssageList(menssagesLista => [
+      ...menssagesLista,
+      {
+        author: 'Me',
+        body: event,
+      },
+    ]);
   };
 
-  render() {
-    return (
-      <div className="bg-sinsiblue w-screen h-screen flex justify-center">
-        <div className="w-screen lg:w-1/2 border border-gray-700 flex flex-col justify-end bg-dots">
-          <div className="h-auto overflow-scroll">
-            <MessageList messages={this.state.messages} />
-            {this.state.boton == true && (
-              <ButtonList
-                onButtonClick={this.handleButtonClick}
-                buttons={this.state.sinsiText['saltoTemporal'].preguntas}
-              />
-            )}
-            <MessageForm onMessageSend={this.handleNewMessage} />
-            {/* <div className="p-6"></div> */}
-          </div>
+  useEffect(() => {
+    getIntention('dame la entrada');
+  }, []);
+
+  const handleNewMessage = text => {
+    setMenssageList(menssagesLista => [
+      ...menssagesLista,
+      {
+        author: 'Me',
+        body: text,
+      },
+    ]);
+    getIntention(text);
+  };
+
+  return (
+    <div className="bg-sinsiblue w-screen h-screen flex justify-center">
+      <div className="w-screen lg:w-1/2 border border-gray-700 flex flex-col justify-end bg-dots">
+        <div className="h-auto overflow-scroll">
+          <MessageList messages={menssagesLista} />
+          {true == true && (
+            <ButtonList
+              onButtonClick={handleButtonClick}
+              buttons={sinsiText['saltoTemporal'].preguntas}
+            />
+          )}
+          <MessageForm onMessageSend={handleNewMessage} />
+          {/* <div className="p-6"></div> */}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }

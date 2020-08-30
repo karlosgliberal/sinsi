69680b3c9c3f171ef6deec88d142b41de9af7616
karlosgliberal <karlosgliberal@gmail.com>
2020-08-30 13:37:06 +0200

    Seguimos siendo funcionales

--- componets/messageList.js
+++ componets/messageList.js
@@ -1,20 +1,11 @@
 import Message from './Message';
 
-export default class MessageList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidUpdate = () => {
-    this.node.scrollTop = this.node.scrollHeight;
-  };
-  render() {
-    return (
-      <div ref={node => (this.node = node)}>
-        {this.props.messages.map((message, i) => (
-          <Message key={i} {...message} />
-        ))}
-      </div>
-    );
-  }
+export default function MessageList(props) {
+  return (
+    <div>
+      {props.messages.map((message, i) => (
+        <Message key={i} {...message} />
+      ))}
+    </div>
+  );
 }
